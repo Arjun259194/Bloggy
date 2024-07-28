7f5b90900316c45d97480c6170cfdc8669a0aeb6
Arjun259194 <arjun259194@gmail.com>
2024-07-29 02:35:23 +0530

    admin panel done

--- app/(auth)/dashboard/admin/page.tsx
+++ app/(auth)/dashboard/admin/page.tsx
@@ -1,20 +1,17 @@
 import BlogManagement from "@/components/dashboard/admin/BlogManager";
-import CategoryManagement from "@/components/dashboard/admin/CatagoriesManager";
 import UserManagement from "@/components/dashboard/admin/UserManager";
 import prisma from "@/lib/db";
 
 const page = async () => {
-  const [blogs, categories, users] = await Promise.all([
+  const [blogs, users] = await Promise.all([
     prisma.blog.findMany({ include: { category: true, uploader: true } }),
-    prisma.category.findMany({ include: { subcategories: true } }),
     prisma.user.findMany(),
   ]);
 
   return (
-    <div className="space-y-5 md:grid md:gap-4 md:grid-cols-2">
+    <div className="space-y-5">
       <UserManagement users={users} />
       <BlogManagement blogs={blogs} />
-      <CategoryManagement categories={categories} />
     </div>
   );
 };
